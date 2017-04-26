e8cbac49fbdb560991efae94c678e9bee8901a45
Petro Korienev <soxjke@gmail.com>
2017-04-26 15:29:25 +0300

    Code-style

--- fastlane/lib/fastlane/actions/sandbox_build.rb
+++ fastlane/lib/fastlane/actions/sandbox_build.rb
@@ -5,33 +5,33 @@ module Fastlane
     end
 
     # Copies your project's artifacts into temprorary sandbox folder and runs specified build actions inside
-    class SandboxBuildAction < Action    	    	
+    class SandboxBuildAction < Action            
       def self.run(params)
-      	self.parse_params(params)
-				self.initialize_project_dir
-				self.create_build_dir_if_needed_and_set_as_working
-				self.generate_build_uuid
-				self.copy_artifacts_to_build_folder
-				self.navigate_to_fastlane
-				self.safely_call_worker_block
+        self.parse_params(params)
+        self.initialize_project_dir
+        self.create_build_dir_if_needed_and_set_as_working
+        self.generate_build_uuid
+        self.copy_artifacts_to_build_folder
+        self.navigate_to_fastlane
+        self.safely_call_worker_block
       end
 
       def self.description
         [
-        	"Copies your project's artifacts into temprorary sandbox folder",
-         	"and runs specified build actions inside"
+          "Copies your project's artifacts into temprorary sandbox folder",
+           "and runs specified build actions inside"
         ].join("\n")
       end
 
       def self.details
         [
-        	"Copies your project's artifacts into temprorary sandbox folder",
-         	"and runs specified build actions inside",
-         	"Build actions are passed via mandatory `worker_block` parameter",
-         	"There's also an optional boolean `clean` parameter which defaults to true",
-         	"If `clean` is false, temporary folder is preserved after completion",
-         	"This action should not be run when current working directory is changed from",
-         	"fastlane's default one"
+          "Copies your project's artifacts into temprorary sandbox folder",
+           "and runs specified build actions inside",
+           "Build actions are passed via mandatory `worker_block` parameter",
+           "There's also an optional boolean `clean` parameter which defaults to true",
+           "If `clean` is false, temporary folder is preserved after completion",
+           "This action should not be run when current working directory is changed from",
+           "fastlane's default one"
         ].join("\n")
       end
 
@@ -47,23 +47,23 @@ module Fastlane
 
       def self.example_code
         [
-        	[
-        		"sandbox_build(worker_block: {",
-        		"  ensure_git_status_clean",
-        		"  gym(scheme: \"Debug\", workspace: \"MyApp.xcworkspace\")",
-        		"  testflight",        		
-        		"})"
-        	].join("\n"),
-        	[
-        		"sandbox_build(",
-        		"  clean: false,",
-        		"  worker_block: {",
-        		"    ensure_git_status_clean",
-        		"    gym(scheme: \"Debug\", workspace: \"MyApp.xcworkspace\")",
-        		"    testflight",        		
-        		"  }",
-        		")"
-        	].join("\n"),        	
+          [
+            "sandbox_build(worker_block: {",
+            "  ensure_git_status_clean",
+            "  gym(scheme: \"Debug\", workspace: \"MyApp.xcworkspace\")",
+            "  testflight",            
+            "})"
+          ].join("\n"),
+          [
+            "sandbox_build(",
+            "  clean: false,",
+            "  worker_block: {",
+            "    ensure_git_status_clean",
+            "    gym(scheme: \"Debug\", workspace: \"MyApp.xcworkspace\")",
+            "    testflight",            
+            "  }",
+            ")"
+          ].join("\n"),          
         ]
       end
 
@@ -71,9 +71,9 @@ module Fastlane
         :building
       end
       
-			def self.step_text
-  	    "Sandboxing build"
-	    end      
+      def self.step_text
+        "Sandboxing build"
+      end      
       
       def self.available_options
         [
@@ -95,62 +95,62 @@ module Fastlane
       end
             
 # Helper
-			BUILD_DIR_NAME = "build"
-			EXCLUDE_PATTERNS = [BUILD_DIR_NAME, ".", "..", ".DS_Store"]
-			FASTLANE_DIR_NAME = "fastlane"
-			
-			def self.parse_params(params)
-      	@clean = params[:clean].nil? ? true : params[:clean]
-      	@worker_block = params[:worker_block]
-      	if @worker_block.nil?
-      		UI.user_error!("sandbox_build called without worker_block specified")      	
-      	end
-			end
-			
-			def self.initialize_project_dir
-				@initial_working_dir = Dir.new(Dir.pwd)
-				if Dir.pwd.split('/').last == FASTLANE_DIR_NAME				
-					Dir.chdir("..")
-				elsif not Dir.exist?(FASTLANE_DIR_NAME)
-      		UI.user_error!("sandbox_build called from non-project and non-fastlane directory")
-      	end				
-	   		@project_dir = Dir.new(Dir.pwd)			
-			end
-			
-			def self.create_build_dir_if_needed_and_set_as_working
-				if not Dir.exist?(BUILD_DIR_NAME)
-					Dir.mkdir(BUILD_DIR_NAME)
-				end
-				Dir.chdir(BUILD_DIR_NAME)
-			end
+      BUILD_DIR_NAME = "build"
+      EXCLUDE_PATTERNS = [BUILD_DIR_NAME, ".", "..", ".DS_Store"]
+      FASTLANE_DIR_NAME = "fastlane"
+      
+      def self.parse_params(params)
+        @clean = params[:clean].nil? ? true : params[:clean]
+        @worker_block = params[:worker_block]
+        if @worker_block.nil?
+          UI.user_error!("sandbox_build called without worker_block specified")        
+        end
+      end
+      
+      def self.initialize_project_dir
+        @initial_working_dir = Dir.new(Dir.pwd)
+        if Dir.pwd.split('/').last == FASTLANE_DIR_NAME        
+          Dir.chdir("..")
+        elsif not Dir.exist?(FASTLANE_DIR_NAME)
+          UI.user_error!("sandbox_build called from non-project and non-fastlane directory")
+        end        
+         @project_dir = Dir.new(Dir.pwd)      
+      end
+      
+      def self.create_build_dir_if_needed_and_set_as_working
+        if not Dir.exist?(BUILD_DIR_NAME)
+          Dir.mkdir(BUILD_DIR_NAME)
+        end
+        Dir.chdir(BUILD_DIR_NAME)
+      end
 
-			def self.generate_build_uuid
-				@build_unique_id = SecureRandom.uuid
-				Actions.lane_context[SharedValues::SANDBOX_BUILD_UUID] = @build_unique_id
-			end
-			
-			def self.copy_artifacts_to_build_folder
-				Dir.mkdir(@build_unique_id)
-				@project_dir.each {|entry|
-					FileUtils.cp_r("#{@project_dir.path}/#{entry}", "#{@build_unique_id}/#{entry}") unless EXCLUDE_PATTERNS.include?(entry)
-				}				
-			end
-			
-			def self.navigate_to_fastlane
-				Dir.chdir(@build_unique_id)
-				Dir.chdir(FASTLANE_DIR_NAME)			
-			end
+      def self.generate_build_uuid
+        @build_unique_id = SecureRandom.uuid
+        Actions.lane_context[SharedValues::SANDBOX_BUILD_UUID] = @build_unique_id
+      end
+      
+      def self.copy_artifacts_to_build_folder
+        Dir.mkdir(@build_unique_id)
+        @project_dir.each {|entry|
+          FileUtils.cp_r("#{@project_dir.path}/#{entry}", "#{@build_unique_id}/#{entry}") unless EXCLUDE_PATTERNS.include?(entry)
+        }        
+      end
+      
+      def self.navigate_to_fastlane
+        Dir.chdir(@build_unique_id)
+        Dir.chdir(FASTLANE_DIR_NAME)      
+      end
       
       def self.safely_call_worker_block
-				begin
-					@worker_block.call()
-				ensure
-					Dir.chdir(@project_dir.path)
-					if @clean
-						FileUtils.remove_dir("#{BUILD_DIR_NAME}/#{@build_unique_id}")
-					end
-					Dir.chdir(@initial_working_dir.path)	
-				end      
+        begin
+          @worker_block.call()
+        ensure
+          Dir.chdir(@project_dir.path)
+          if @clean
+            FileUtils.remove_dir("#{BUILD_DIR_NAME}/#{@build_unique_id}")
+          end
+          Dir.chdir(@initial_working_dir.path)  
+        end      
       end
     end
   end
